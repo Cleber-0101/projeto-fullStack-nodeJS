4d08751a593a6b44a2d643c2b41c4b8f9b8e3d79
Cleber-0101 <cleberbatista99@hotmail.com>
2026-05-24 00:17:09 -0300

    Estruturando projeto

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-let express = require('express')
-let app = express()
+const express = require('express');
+const app = express();
+
+const PORTA = 3000
+
+//escutando 
+app.listen(PORTA, function(){
+    console.log(`Estou entrando ${PORTA}`)
+});
+
+//criando rota
+app.get('/', (req, res) =>{
+    res.send("Esta funcionando REST")
+});
 
